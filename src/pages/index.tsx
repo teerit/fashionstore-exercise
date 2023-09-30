3ba98222539b6c348848744270b03c717581a770
Teerit Ploensin <teeritploensin@gmail.com>
2023-09-30 16:01:31 +0700

    Refactor with useState variable

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,30 @@
 import ProductList from "@/components/ProductList";
+import { useState } from "react";
 
 export default function Home() {
-  const productName = "";
-  const products = [{ name: "Loose Cropped Jeans (Damaged)" }];
+  const [productName, setProductName] = useState("");
+  const [products, setProducts] = useState([
+    { name: "Loose Cropped Jeans (Damaged)" },
+  ]);
 
   return (
     <>
       <form
         onSubmit={(event) => {
           event.preventDefault();
+          setProducts([...products, { name: productName }]);
         }}
       >
         <h1 className="text-2xl font-bold">Add a new product</h1>
         <div>
           <label htmlFor="product-name">Product name:</label>
-          <input className="bg-slate-200" id="product-name" />
+          <input
+            className="bg-slate-200"
+            id="product-name"
+            onChange={(event) => {
+              setProductName(event.target.value);
+            }}
+          />
           <p data-testid="result">{productName}</p>
         </div>
       </form>
